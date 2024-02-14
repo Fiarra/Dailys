799347d9f4f94038793a46c7dce630783374ea30
Fiarra <104574539+Fiarra@users.noreply.github.com>
2024-02-14 15:00:59 +0100

    Daily 12

---
+++ 24_02_14/settings.py
@@ -0,0 +1,17 @@
+SCRAPEOPS_API_KEY = '43e15d00-02f4-41c1-9c4e-457858dff919'
+
+# Add In The ScrapeOps Monitoring Extension
+EXTENSIONS = {
+'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
+
+
+DOWNLOADER_MIDDLEWARES = {
+
+    ## ScrapeOps Monitor
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    
+    ## Proxy Middleware
+    'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
